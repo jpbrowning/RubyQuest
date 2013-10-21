48ab3fb40e1163c9b9685db56ae5b249ff994a89
joey-delorenzo <joey.delorenzo10@gmail.com>
2013-10-21 19:38:06 -0400

    Create RubyQuest.rb
    v0.2 of RubyQuest....we have a title screen. Fuck yeah.

---
+++ RubyQuest.rb
@@ -0,0 +1,57 @@
+# James Browning - 140726
+# Joey DeLorenzo - 141560
+#
+# Ruby Phase 2
+#
+# 
+
+width = `/usr/bin/env tput cols`.to_i
+
+
+
+
+puts `clear`
+puts width
+$fixedWidth = false
+if width < 60
+	puts "Please extend terminal horizontally!"
+	$fixedWidth = true
+end
+
+while width < 60
+	width = `/usr/bin/env tput cols`.to_i 
+end
+
+
+
+puts "\n"
+
+puts "O-----------------------------------------------O".center(width)
+puts "| (                         )                   |".center(width)
+puts "| )\\ )             (     ( /(       ____________|".center(width)
+puts "|(()/(      (    ( )\\    )\\())     / /        \\ \\".center(width)
+puts " | /(_))     )\\   )((_)  ((_)\\     /_/__________\\_\\".center(width)
+puts " |(_))    _ ((_) ((_)_  __ ((_)    \\ \\          / /".center(width)
+puts "|| _ \\  | | | |  | _ ) \\ \\ / /     \\ \\        / /".center(width)
+puts "||   /  | |_| |  | _ \\  \\ V /       \\ \\      / /|".center(width)
+puts "||_|_\\   \\___/   |___/   |_|         \\ \\    / / |".center(width)
+puts "|                                     \\ \\  / /  |".center(width)
+puts "|                            (         \\ \\/ /   |".center(width)
+puts "|        (                   )\\ )    *  \\  /    |".center(width)
+puts "|      ( )\\       (    (    (()/(  ` )   \\/     |".center(width)
+puts "|      )((_)      )\\   )\\    /(_))  ( )(_       |".center(width)
+puts "|     ((_)_    _ ((_) ((_)  (_))   (_(_())      |".center(width)
+puts "|      / _ \\  | | | | | __| / __|  |_   _|      |".center(width)
+puts "|     | (_) | | |_| | | _|  \\__ \\    | |        |".center(width)
+puts "|      \\__\\_\\  \\___/  |___| |___/    |_|        |".center(width)
+puts "O-----------------------------------------------O".center(width)
+
+if $fixedWidth
+	puts "\n"
+	puts "Thank you for extending your terminal!".center(width)
+	puts "AND NOW........".center(width)
+end
+puts "\nWelcome to Ruby Quest v0.2\n\n"
+puts "Story..."
+
+#puts Map
