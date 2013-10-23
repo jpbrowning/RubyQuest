0a1dc3ac40b9435d05fa520b792a1ea745e084bf
joey-delorenzo <joey.delorenzo10@gmail.com>
2013-10-22 22:24:08 -0400

    Re-made QuestObject.rb

--- QuestObject.rb
+++ QuestObject.rb
@@ -3,29 +3,70 @@
 # Object.rb
 
 
-# So, some my class stuff isn't working properly....
-# This is le broken
-
 class QuestObject
   attr_accessor :obj_name
   attr_accessor :obj_attr
   attr_accessor :obj_secondattr
   
-  def initialize( objn, obja, objsa )
-    $obj_name = objn
-    $obj_attr = obja
-    $obj_secondattr = objsa
-  #end
+  $objs = ["Door", "Chest", "Knife", "Body", "Vial", "Key", "Windows", "Herb"]
+  $doorAttr = ["Old", "New", "Dusty", "Moldy", "Broken", "Creaky"]
+  $doorAttrS = ["White", "Brown", "Maple", "Pine", "Cherry Wood", "Bamboo"]
+  $chestAttr = ["Large", "Small", "Glowing"]
+  $chestAttrS = ["Omnious", "Creepy", "On-Fire"]
+  $knifeAttr = ["Sharp", "Dull"]
+  $knifeAttrS = ["Matte", "Glistening"]
+  $bodyAttr = ["Rotting", "Fresh", "Bloodied"]
+  $bodyAttrS = ["Covered in flies", "Clothed", "Naked"]
+  $vialAttr = ["Large", "Small", "Max"]
+  $vialAttrS = ["Purple", "Red", "Yellow"]
+  $windowAttr = ["Broken", "Clear", "Opaque", "Opened", "Closed"]
+  $windowAttrS = ["Covered", "Barred", "Taped Off", "Large", "Small"]
+  $herbAtt = ["Green", "Yellow", "Red"]
+  $herbAttS = ["Fresh", "Wilted", "Spritely"]
 
-  #def sayObject()
+  
+  def buildObject()
+    x = rand(5)
+
+    objX = $objs[x]
+    case x
+    when 0
+      attrX = $doorAttr[rand(5)]
+      attrSX = $doorAttrS[rand(5)]
+    when 1
+      attrX = $chestAttr[rand(2)]
+      attrSX = $chestAttrS[rand(2)]
+    when 2
+      attrX = $knifeAttr[rand(1)] 
+      attrSX = $knifeAttrS[rand(1)]
+    when 3
+      attrX = $bodyAttr[rand(2)]
+      attrSX = $bodyAttrS[rand(2)]
+    when 4
+      attrX = $vialAttr[rand(2)]
+      attrSX = $vialAttrS[rand(2)]  
+    when 5
+      attrX = $windowAttr[rand(4)] 
+      attrSX = $windowAttrS[rand(4)] 
+    when 6
+      attrX = $herbAttr[rand(2)]
+      attrSX = $herbAttrS[rand(2)]
+    end
+
+    $obj_name = objX
+    $obj_attr = attrX
+    $obj_secondattr = attrSX
     print $obj_name
     print " which is "
     print $obj_attr
     print " & "
     puts $obj_secondattr
+
   end
-  
 end
 
 
 
+
+
+#this is a test
