c04b0b3937d17e1b8a84b5f7cf8fc09df992ee4b
James Browning <Jamboix@gmail.com>
2013-10-24 09:23:46 -0400

    Create Game.rb
    The basic game class. Just initializes and makes the first room.

---
+++ Game.rb
@@ -0,0 +1,17 @@
+# James Browning
+# Joey De Lorenzo
+
+# Object.rb
+
+require "Room"
+require "QuestObject"
+
+class Game
+
+	def initialize()
+		puts "Welcome to the dungeon. You open the front door and enter the first room. In this room, you look around and see:"
+		
+		room1 = Room.new(rand(4))
+		room1.buildRoom
+	end
+end
