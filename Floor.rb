b4db62622dd02c544fad8fff2801712db3eff51d
joey-delorenzo <joey.delorenzo10@gmail.com>
2013-10-24 09:33:00 -0400

    Create Floor.rb

---
+++ Floor.rb
@@ -0,0 +1,33 @@
+# @author Joey De Lorenzo
+# @author Trey Browning
+
+# Floor.rb
+
+# This class handles a floor of our text adventure game
+# Each floor will contain a rando numba of floors.
+
+
+require "Room"
+require "Game"
+require "QuestObject"
+
+
+class Floor
+	attr_accessor :room_arr
+	attr_accessor :num_of_rooms
+
+	def initialize( x )
+		$num_of_rooms = x
+		$room_arr = Array.new
+	end
+
+	def buildFloor( ) 
+		i = 0;
+		while i < $num_of_rooms
+			thisRoom = Room.new(1)
+			thisRoom.buildRoom
+			$room_arr << thisRoom
+			i = i + 1;
+		end
+	end
+end
