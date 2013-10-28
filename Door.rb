bfd860f08bcf971e9d69cb8f45ab5d8b972adcad
James Browning <Jamboix@gmail.com>
2013-10-28 17:19:58 -0400

    Update Door.rb

--- Door.rb
+++ Door.rb
@@ -7,7 +7,14 @@ class Door
   end 
   
   def open()
-      @nextRoom.enter()
+  		if(@nextRoom.class.name == "Room")
+      	@nextRoom.enter()
+      elsif(@nextRoom.class.name == "Array" && @nextRoom.size > 0)
+      	nextFloor = @nextRoom.shift
+      	nextFloor.buildFloor(@nextRoom)
+      	nextFloor.start()
+      else
+      	puts "You have reached the end."
+      end
   end
-  
 end
