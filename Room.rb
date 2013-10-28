a3012471e7d581c41091b8065cf6d88285f82538
James Browning <Jamboix@gmail.com>
2013-10-28 15:46:51 -0400

    Update Room.rb

--- Room.rb
+++ Room.rb
@@ -13,32 +13,32 @@ require "Door"
 
 class Room
   attr_accessor :objs_arr
-  $num_objs
-  $lastRoom
-  $door
+  @num_objs
+  @lastRoom
+  @door
   
   def initialize(x)
-    $num_objs = x
-    $objs_arr = Array.new(x) { QuestObject.new }
+    @num_objs = x
+    @objs_arr = Array.new(x) { QuestObject.new }
   end
     
   def buildRoom(l, n)
     i = 0
     
-    $lastRoom = l
-    $door = Door.new
-    $door.buildObject(n)    
+    @lastRoom = l
+    @door = Door.new
+    @door.buildObject(n)    
     
-    for obj in $objs_arr
+    for obj in @objs_arr
       obj.buildObject
     end
   end
   
   def enter()
   	puts "You enter a room. Would you like to look around? "
-  	$answer = gets.chomp
+  	@answer = gets.chomp
   	
-  	if $answer.to_s == "yes"
+  	if @answer.to_s == "yes"
   		lookAround()
   	end
   end
@@ -46,18 +46,18 @@ class Room
   def lookAround()
    i = 0
    
-   if $num_objs.to_i == 0
+   if @num_objs.to_i == 0
    	puts "There is nothing interesting in this room."
    end
    
-  	for obj in $objs_arr
+  	for obj in @objs_arr
   		obj.inspect()
   	end
    
    puts "Would you like to open the door?"
-   	$answer = gets.chomp
-   	if $answer.to_s == "yes"
-  			$door.open()
+   	@answer = gets.chomp
+   	if @answer.to_s == "yes"
+   		@door.open
   		end
   end
 end
