05e9f70ff0d4312ea2e5edf952b4d6669fa191de
James Browning <Jamboix@gmail.com>
2013-10-28 11:14:46 -0400

    Update Floor.rb

--- Floor.rb
+++ Floor.rb
@@ -4,7 +4,7 @@
 # Floor.rb
 
 # This class handles a floor of our text adventure game
-# Each floor will contain a rando numba of floors.
+# Each floor will contain a rando number of floors.
 
 
 require "Room"
@@ -12,21 +12,27 @@ require "QuestObject"
 
 
 class Floor
-        attr_accessor :room_arr
-        attr_accessor :num_of_rooms
+	attr_accessor :room_arr
+	attr_accessor :num_of_rooms
 
-        def initialize( x )
-                $num_of_rooms = x
-                $room_arr = Array.new
-        end
+	def initialize( x )
+		$num_of_rooms = x		
+		$room_arr = Array.new(x) { Room.new(rand(3)) }
+	end
 
-        def buildFloor( )
-                i = 0;
-                while i < $num_of_rooms
-                        thisRoom = Room.new(1)
-                        thisRoom.buildRoom
-                        $room_arr << thisRoom
-                        i = i + 1;
-                end
-        end
+	def buildFloor( )
+		i = 1;
+		puts $num_of_rooms
+		$room_arr[0].buildRoom(nil, $room_arr[1])
+		while i < $num_of_rooms.to_i-1
+			$room_arr[i].buildRoom($room_arr[i-1], $room_arr[i+1])
+			i = i + 1;
+		end
+		$room_arr[i].buildRoom($room_arr[1], nil)
+		
+	end
+	
+	def start()
+		$room_arr[0].enter()
+	end
 end
