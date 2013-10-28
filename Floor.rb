774e92fad9f7958ae2e7291d62aca7ddb9cbf0f3
James Browning <Jamboix@gmail.com>
2013-10-28 15:47:01 -0400

    Update Floor.rb

--- Floor.rb
+++ Floor.rb
@@ -12,27 +12,29 @@ require "QuestObject"
 
 
 class Floor
-	attr_accessor :room_arr
-	attr_accessor :num_of_rooms
-
-	def initialize( x )
-		$num_of_rooms = x		
-		$room_arr = Array.new(x) { Room.new(rand(3)) }
+	def initialize(x)	
+		@room_arr = Array.new(x)
+		@num_of_rooms = x
+		j = 0
+		while j < x
+			@room_arr[j] = Room.new(rand(3))
+			j = j + 1
+		end		
 	end
 
 	def buildFloor( )
 		i = 1;
-		puts $num_of_rooms
-		$room_arr[0].buildRoom(nil, $room_arr[1])
-		while i < $num_of_rooms.to_i-1
-			$room_arr[i].buildRoom($room_arr[i-1], $room_arr[i+1])
+		
+		@room_arr[0].buildRoom(nil, @room_arr[1])
+		while i < @num_of_rooms.to_i-1
+			@room_arr[i].buildRoom(@room_arr[i-1], @room_arr[i+1])
 			i = i + 1;
 		end
-		$room_arr[i].buildRoom($room_arr[1], nil)
+		@room_arr[i].buildRoom(@room_arr[1], nil)
 		
 	end
 	
 	def start()
-		$room_arr[0].enter()
+		@room_arr[0].enter()
 	end
 end
