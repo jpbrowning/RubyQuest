9652a4f903b628a812157f8ab7440823192dbc9d
James Browning <Jamboix@gmail.com>
2013-10-28 11:14:33 -0400

    Update Game.rb

--- Game.rb
+++ Game.rb
@@ -7,17 +7,18 @@
 require "Floor"
 
 class Game
-        attr_accessor :protagonist
-        attr_accessor :progress
-        attr_accessor :floor_arr
-        #attr_accessor :
+	attr_accessor :protagonist
+	attr_accessor :progress
+	attr_accessor :floor_arr
+	#attr_accessor :
 
-        def initialize()
-                floor_arr = Array.new
-        end
+	def initialize()
+		floor_arr = Array.new
+	end
 
-        def buildGame()
-                thisFloor = Floor.new(3)
-                thisFloor.buildFloor
-        end
+	def buildGame()
+		thisFloor = Floor.new(3)
+		thisFloor.buildFloor
+		thisFloor.start()
+	end
 end
