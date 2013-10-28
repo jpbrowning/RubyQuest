399c8fd5ef87f3024c8f7a9de9002d45093364ae
James Browning <Jamboix@gmail.com>
2013-10-28 11:13:54 -0400

    Update Room.rb

--- Room.rb
+++ Room.rb
@@ -9,29 +9,55 @@
 # within the room are never the same.
 
 require "QuestObject"
+require "Door"
 
 class Room
   attr_accessor :objs_arr
   $num_objs
-
-
-  def initialize( x )
+  $lastRoom
+  $door
+  
+  def initialize(x)
     $num_objs = x
-    $objs_arr = Array.new($num_objs)
+    $objs_arr = Array.new(x) { QuestObject.new }
   end
     
-  def buildRoom()
+  def buildRoom(l, n)
     i = 0
     
-    if $num_objs == 0 		#Handle empty room
-    	puts "Nothing particularly interesting."
-    end
+    $lastRoom = l
+    $door = Door.new
+    $door.buildObject(n)    
     
-    while i < $num_objs
-      q = QuestObject.new
-      $objs_arr << q.buildObject
-      i = i + 1
+    for obj in $objs_arr
+      obj.buildObject
     end
-    
+  end
+  
+  def enter()
+  	puts "You enter a room. Would you like to look around? "
+  	$answer = gets.chomp
+  	
+  	if $answer.to_s == "yes"
+  		lookAround()
+  	end
+  end
+  
+  def lookAround()
+   i = 0
+   
+   if $num_objs.to_i == 0
+   	puts "There is nothing interesting in this room."
+   end
+   
+  	for obj in $objs_arr
+  		obj.inspect()
+  	end
+   
+   puts "Would you like to open the door?"
+   	$answer = gets.chomp
+   	if $answer.to_s == "yes"
+  			$door.open()
+  		end
   end
 end
