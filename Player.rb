de54648cbf018cf2e98c83b38ebb65316aaffcf8
James Browning <Jamboix@gmail.com>
2013-10-30 15:55:06 -0400

    Update Player.rb
    Formatting and comments.

--- Player.rb
+++ Player.rb
@@ -7,52 +7,57 @@
 
 require "QuestObject"
 
-class Player
-  @name
-  @level
-  @hp
-  @mp
-  @items
-  
-  # Create player at level 1.
-  def initialize(name)
-    @name = name
-    @level = 1
-    @hp = 20
-    @mp = 10
-    @items = []
-  end
-  
-  # Return Player's name
-  def getName()
-    return @name
-  end
-  
-  # Subtract the damage taken from the Player's total HP
-  def takeHit(x)
-    @hp = @hp - x
-    return @hp
-  end
-  
-  # Return a number based on two times the player level +/- 2.
-  def attack()
-    return @level * 2 + rand(3) - rand(3)
-  end
-  
-  # Pick up a QuestObject from a Room
-  def pickUp(i)
-    @items << i
-  end
-  
-  # Output the content of the Player's inventory to the screen.
-  def checkItems()
-    if @items.size == 0
-      puts "\nYour inventory is empty"
-    else
-      puts "\nYour inventory contains: "
-      for item in @items
-        item.inspect()
+   class Player
+   @name
+   @level
+   @hp
+   @mp
+   @items
+
+   # Create player at level 1.
+   def initialize(name)
+      @name = name
+      @level = 1
+      @hp = 20
+      @mp = 10
+      @items = []
+   end
+
+   # Return Player's name
+   def getName()
+      return @name
+   end
+
+   # Subtract the damage taken from the Player's total HP
+   def takeHit(x)
+      @hp = @hp - x
+      return @hp
+   end
+
+   # Returns the Player's health.
+   def getHealth()
+      return @hp
+   end
+
+   # Return a number based on two times the player level +/- 2.
+   def attack()
+      return @level * 2 + rand(3) - rand(3)
+   end
+
+   # Pick up a QuestObject from a Room.
+   def pickUp(i)
+      @items << i
+   end
+
+   # Output the content of the Player's inventory to the screen.
+   def checkItems()
+      if @items.size == 0
+         puts "\nYour inventory is empty"
+      else
+         puts "\nYour inventory contains: "
+         for item in @items
+         item.inspect()
+         end
       end
-    end
-  end
+   end
 end
