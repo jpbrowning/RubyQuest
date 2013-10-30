a7046bc84c477e7179f3977de7ef6f71f7c14751
James Browning <Jamboix@gmail.com>
2013-10-30 15:53:47 -0400

    Update Floor.rb
    Formatting and comments.

--- Floor.rb
+++ Floor.rb
@@ -6,33 +6,34 @@
 # This class handles a floor of our text adventure game
 # Each floor will contain a random number of floors.
 
-
 require "Room"
 
-
 class Floor
-	def initialize(x)	
-		@room_arr = Array.new(x)
-		@num_of_rooms = x
-		j = 0
-		while j < x
-			@room_arr[j] = Room.new(rand(3)+1)
-			j = j + 1
-		end		
-	end
 
-	def buildFloor(floor_arr)
-		i = 1;
-		@room_arr[0].buildRoom(nil, @room_arr[1])
-		while i < @num_of_rooms.to_i-1
-			@room_arr[i].buildRoom(@room_arr[i-1], @room_arr[i+1])
-			i = i + 1;
-		end
-		@room_arr[i].buildRoom(@room_arr[1], floor_arr)
-		
-	end
-	
-	def start(player)
-		@room_arr[0].enter(player)
-	end
+   # Initializes the Floor, with a random number of Rooms.
+   def initialize(x)	
+      @room_arr = Array.new(x)
+      @num_of_rooms = x
+      j = 0
+      while j < x
+         @room_arr[j] = Room.new(rand(3))
+         j = j + 1
+      end		
+   end
+
+   # Builds the floor, and passes the other floors to the last Room.
+   def buildFloor(floor_arr)
+      i = 1;
+      @room_arr[0].buildRoom(nil, @room_arr[1])
+      while i < @num_of_rooms.to_i-1
+         @room_arr[i].buildRoom(@room_arr[i-1], @room_arr[i+1])
+         i = i + 1;
+      end
+      @room_arr[i].buildRoom(@room_arr[1], floor_arr)
+   end
+
+   # Passes the Player into the first room on the Floor.
+   def start(player)
+      @room_arr[0].enter(player)
+   end
 end
