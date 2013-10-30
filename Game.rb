34fb9f1dfcee580298307e3ea4c26dce62e00843
James Browning <Jamboix@gmail.com>
2013-10-30 15:54:11 -0400

    Update Game.rb
    Formatting and comments.

--- Game.rb
+++ Game.rb
@@ -8,31 +8,34 @@ require "Floor"
 require "Player"
 
 class Game
-	def initialize
-		puts `clear`
-		width = `/usr/bin/env tput cols`.to_i
-		
-		printf("What is your name? ")
-		@name = gets.chomp
-		@player = Player.new(@name)
-		
-		puts `clear`
-		
-		puts "\nWelcome to Ruby Quest, %s.\n\n\n".center(width) % @player.getName()
-		
-		puts "\tAfter what must have been days, you finally wake up"
-		puts "\tonly to find yourself in front of an old, rotting door.\n\n"
-		puts "\tYou slowly open the door, and step forward...\n\n"
-		puts "[PRESS ANY KEY]".center(width)
-		system("stty raw -echo")
-  		STDIN.getc
-  		system("stty -raw echo")
-		@floor_arr = Array.new(3) { Floor.new(3) }
-	end
+   
+   # Initializes a new game, and builds the Player.
+   def initialize()
+      puts `clear`
+      width = `/usr/bin/env tput cols`.to_i
 
-	def startGame()
-		floor = @floor_arr.shift
-		floor.buildFloor(@floor_arr)
-		floor.start(@player)
-	end
+      printf("What is your name? ")
+      @name = gets.chomp
+      @player = Player.new(@name)
+
+      puts `clear`
+
+      puts "\nWelcome to Ruby Quest, %s.\n\n\n".center(width) % @player.getName()
+
+      puts "\tAfter what must have been days, you finally wake up"
+      puts "\tonly to find yourself in front of an old, rotting door.\n\n"
+      puts "\tYou slowly open the door, and step forward...\n\n"
+      puts "[PRESS ANY KEY]".center(width)
+      system("stty raw -echo")
+      STDIN.getc
+      system("stty -raw echo")
+      @floor_arr = Array.new(3) { Floor.new(3) }
+   end
+
+   # Starts the game, passing the Player into the first floor.
+   def startGame()
+      floor = @floor_arr.shift
+      floor.buildFloor(@floor_arr)
+      floor.start(@player)
+   end
 end
