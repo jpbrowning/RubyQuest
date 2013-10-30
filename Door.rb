64f2f7fe84ed9733189e9d21b70a3925c1a8c6c6
James Browning <Jamboix@gmail.com>
2013-10-30 15:53:26 -0400

    Update Door.rb
    Formatting and comments.

--- Door.rb
+++ Door.rb
@@ -1,29 +1,23 @@
 # @author Joey De Lorenzo
 # @author Trey Browning
 
-class Door  
-  def buildObject(n)  
-                  @nextRoom = n
-  end 
-  
-  def open(player)
-      width = `/usr/bin/env tput cols`.to_i
-    
+class Door
+
+   # Initializes a Door, leading to the next Room.
+   def buildObject(n)  
+      @nextRoom = n
+   end 
+
+   # Passes the Player into the next room, or floor.
+   def open(player)
       if(@nextRoom.class.name == "Room")
-              @nextRoom.enter(player)
+         @nextRoom.enter(player)
       elsif(@nextRoom.class.name == "Array" && @nextRoom.size > 0)
-              nextFloor = @nextRoom.shift
-              puts "This door is leads to a stair case..."
-              puts "You walk up the dark, dannk staircase onto the next floor..."
-              puts "[PRESS ANY KEY]".center(width)
-              system("stty raw -echo")
-              STDIN.getc
-              system("stty -raw echo")
-              
-              nextFloor.buildFloor(@nextRoom)
-              nextFloor.start(player)
+         nextFloor = @nextRoom.shift
+         nextFloor.buildFloor(@nextRoom)
+         nextFloor.start(player)
       else
-              puts "You have reached the end."
+         puts "You have reached the end."
       end
-  end
+   end
 end
