7ce6fc55cc5fc76576a1ec7da55e00057ae81f68
James Browning <Jamboix@gmail.com>
2013-10-29 23:17:44 -0400

    Update Door.rb

--- Door.rb
+++ Door.rb
@@ -2,22 +2,19 @@
 # @author Trey Browning
 
 class Door  
-  
-  # Pass the next room in.
   def buildObject(n)  
-  		@nextRoom = n
+                  @nextRoom = n
   end 
   
-  # Go to where the Door leads, be it a Room or an Array of Rooms (a floor)
   def open()
-  		if(@nextRoom.class.name == "Room")
-      	@nextRoom.enter()
+                  if(@nextRoom.class.name == "Room")
+              @nextRoom.enter()
       elsif(@nextRoom.class.name == "Array" && @nextRoom.size > 0)
-      	nextFloor = @nextRoom.shift
-      	nextFloor.buildFloor(@nextRoom)
-      	nextFloor.start()
+              nextFloor = @nextRoom.shift
+              nextFloor.buildFloor(@nextRoom)
+              nextFloor.start()
       else
-      	puts "You have reached the end."
+              puts "You have reached the end."
       end
   end
 end
