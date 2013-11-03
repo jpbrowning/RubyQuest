48e5095dd3f460ddb3305b8ca1d2ee99172d288d
joey-delorenzo <joey.delorenzo10@gmail.com>
2013-11-03 17:02:39 -0500

    Update Door.rb

--- Door.rb
+++ Door.rb
@@ -2,17 +2,33 @@
 # @author Trey Browning
 
 class Door
+   width = `/usr/bin/env tput cols`.to_i
 
+   
    # Initializes a Door, leading to the next Room.
    def buildObject(n)  
       @nextRoom = n
    end 
 
    # Passes the Player into the next room, or floor.
+   def checkNext()
+      if(@nextRoom.class.name == "Room")
+	return 0
+      elsif(@nextRoom.class.name == "Array" && @nextRoom.size > 0)
+	return 1
+      else 
+	return 2
+      end
+   end
+
    def open(player)
       if(@nextRoom.class.name == "Room")
          @nextRoom.enter(player)
       elsif(@nextRoom.class.name == "Array" && @nextRoom.size > 0)
+	 puts `clear`
+	 puts "You ascend the stairs to the next floor"
+	 puts "Please wait....."
+	 sleep(1)
          nextFloor = @nextRoom.shift
          nextFloor.buildFloor(@nextRoom)
          nextFloor.start(player)
