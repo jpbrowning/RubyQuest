8847c33e922f6940ce9f2c9e98a5be455c159ac7
joey-delorenzo <joey.delorenzo10@gmail.com>
2013-11-05 08:18:30 -0500

    Update Player.rb

--- src/Player.rb
+++ src/Player.rb
@@ -7,7 +7,12 @@
 
 require "QuestObject"
 
-class Player
+   class Player
+   @name
+   @level
+   @hp
+   @mp
+   @items
 
    # Create player at level 1.
    def initialize(name)
@@ -17,7 +22,21 @@ class Player
       @mp = 10
       @items = []
    end
+   
+   ### Taken from http://stackoverflow.com/questions/1489183/colorized-ruby-output########
+   def colorize(text, color_code)							##
+     "\e[#{color_code}m#{text}\e[0m"							##
+   end											##
+											##
+											##
+   def red(text); colorize(text, 31); end						##
+   def green(text); colorize(text, 32); end						##
+   #######################################################################################
 
+   def pink(text); colorize(text, 35); end
+   def yellow(text); colorize(text, 33); end
+   def magenta(text); colorize(text, 45); end
+   def cyan(text); colorize(text, 36); end
    # Return Player's name
    def getName()
       return @name
@@ -50,6 +69,8 @@ class Player
       return 30
      elsif q == 2
        return 10
+     elsif q == 9000
+       return 9000
      end
    end
 
@@ -71,7 +92,7 @@ class Player
 
    def useItemMenu() 
      if @items.size == 0
-       puts "\nYour inventory is empty!"
+       puts "\nYour inventory is empty, there is nothing to use! You've wasted a turn!"
        puts "\n"
        return 0;
      elsif
@@ -97,51 +118,68 @@ class Player
 	puts "This isn't combat usable!"
 	puts "......"
 	puts "Hell-- why would you even try that?!"
+	return 0
 
       elsif @items[@answer].getName() == "Herb" or @items[@answer].getName() == "Vial" 
 	if @items[@answer].getFAttr() == "Green"
 	  @hp = @hp + 5
-	  puts "Your HP has increased by 5!"
+	  puts `clear`
+	  puts red("Your HP has increased by 5!")
 	elsif @items[@answer].getFAttr() == "Yellow"
 	  @hp = @hp + 10
-	  puts "Your HP has increased by 10!"
+	  puts `clear`
+	  puts red("Your HP has increased by 10!")
 	elsif @items[@answer].getFAttr() == "Red"
 	  @hp = @hp + 15
-	  puts "Your HP has increased by 15!"
+	  puts `clear`
+	  puts red("Your HP has increased by 15!")
 	elsif @items[@answer].getFAttr() == "Max"
 	  @hp = @hp + 15
-	  puts "Your HP has increased by 15!"
+	  puts `clear`
+	  puts red("Your HP has increased by 15!")
 	elsif @items[@answer].getFAttr() == "Large"
 	  @hp = @hp + 10
-	  puts "Your HP has increased by 10!"
+	  puts `clear`
+	  puts red("Your HP has increased by 10!")
 	elsif @items[@answer].getFAttr() == "Small"
 	  @hp = @hp +5
-	  puts "Your HP has increased by 5!"
+	  puts `clear`
+	  puts red("Your HP has increased by 15!")
 	end
 	if @items[@answer].getSAttr() == "Wilted"
 	  @hp = @hp - 5
-	  puts "Wilted herb deducted 5 hp!"
-	  
+	  puts cyan("Wilted Herb deducts five HP from gain!")
 	elsif @items[@answer].getSAttr() == "Purple"
 	  @level = @level + 1
-	  puts "\n"
-	  puts "Plus 1 Attack Boost!"
+	  puts magenta("Plus 1 Attack Boost!")
 	elsif @items[@answer].getSAttr() == "Yellow"
 	  @level = @level + 2
-	  puts "\n"
-	  puts "Plus 2 Attack Boost!"
+	  puts yellow("Plus 2 Attack Boost!")
 	end
 	@items.delete_at(@answer)
 	return 0;
       elsif @items[@answer].getName() == "Knife"
 	if @items[@answer].getFAttr() == "Sharp"
-	  attack(1);
+	  @items.delete_at(@answer)
+	  #attack(1);
 	  return 1
 	else
-	  attack(2)
+	  @items.delete_at(@answer)
+	  #attack(2)
 	  return 2;
 	end
-	@items.delete_at(@answer)
+      elsif @items[@answer].getName() == "Body"
+	puts `clear`
+	puts green('Body reanimates, the Zombie leaves a gold, shinny object')
+	puts green('in the heat of battle. You pick it up and it feels very familar.')
+	puts green('It\'s bad guy slaying time')
+	puts red("Tuba has been added to your inventory!")
+	puts "\n\n\n\n"
+	puts "You attack with the Tuba!"
+	x = QuestObject.new
+	x.makeTuba()
+	@items[@answer].insert(x)
+	return 9000;
       end
    end
 
@@ -156,15 +194,15 @@ class Player
       x = 1
       if @items.size == 0
          puts "\nYour inventory is empty"
-	      puts "\nYour current HP is: %i" % @hp
+	 puts "\nYour current HP is: %i" % @hp
       else
          puts "\nYour inventory contains: "
          for item in @items
-         	print "\t %i." % x
-         	item.inspect()
-         	x = x + 1
+	  print "\t %i." % x
+	  item.inspect()
+	  x = x + 1
          end
-	     puts "\nYour current HP is: %i" % @hp
+	 puts "\nYour current HP is: %i" % @hp
       end
    end
 
